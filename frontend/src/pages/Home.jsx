000f1e25066220338f0d8212b34b583c5035a046
mjKim <106668821+pipakmj@users.noreply.github.com>
2026-03-11 02:24:59 +0900

    feat: 라우터 추가

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,17 +1,13 @@
-import Navbar from "../components/Navbar";
 import HeroSection from "../components/HeroSection";
 import FeatureSection from "../components/FeatureSection";
 import CTASection from "../components/CTASection";
-import Footer from "../components/Footer";
 
 export default function Home() {
     return (
         <>
-            <Navbar />
             <HeroSection />
             <FeatureSection />
             <CTASection />
-            <Footer />
         </>
     );
 }
